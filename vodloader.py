ca4863ce8dd456a9df42e1abb161d02d5ad9d775
Retrontology <retrontology@hotmail.com>
2021-03-29 21:58:09 -0500

    config

--- vodloader.py
+++ vodloader.py
@@ -3,50 +3,57 @@ from twitchAPI.webhook import TwitchWebHook
 from twitchAPI.oauth import UserAuthenticator
 from twitchAPI.types import AuthScope
 import streamlink
+import vodloader_config
+
+
+class vod_watcher(object):
+
+    def __init__(self, streamer, twitch, hook, quality='best'):
+        self.streamer = streamer
+        self.quality = quality
+        self.twitch = twitch
+        self.hook = hook
+        self.user_id = self.get_user_id()
+        self.webhook_setup()
+
+
+    def callback_stream_changed(self, uuid, data):
+        print('UUID: ' + uuid)
+        print(data)
+
+
+    def webhook_setup(self):
+        success, uuid = self.hook.subscribe_stream_changed(self.user_id, self.callback_stream_changed)
+
+
+    def get_stream(self, quality='best'):
+        url = 'https://www.twitch.tv/' + self.streamer
+        return streamlink.streams(url)[quality]
+
+
+    def get_user_id(self):
+        user_info = self.twitch.get_users(logins=[self.streamer])
+        return user_info['data'][0]['id']
+
+
+    def download_stream(self, path, chunk_size=8192):
+        stream = self.get_stream().open()
+        with open(path, 'wb') as f:
+            data = stream.read(chunk_size)
+            while data:
+                try:
+                    f.write(data)
+                except OSError as err:
+                    print(err)
+                    break
+                data = stream.read(chunk_size)
+        stream.close()
 
 
 def main():
-    webhook_setup()
-
-
-def callback_stream_changed(uuid, data):
-    print('UUID: ' + uuid)
-    print(data)
-
-
-def webhook_setup():
     twitch = Twitch(clientID, clientSecret)
     twitch.authenticate_app([])
-    user_info = twitch.get_users(logins=users)
-    user_id = user_info['data'][0]['id']
-    hook = TwitchWebHook(host, clientID, port)
+    hook = TwitchWebHook(host, twitch_client_id, port)
+    hook.authenticate(twitch)
     hook.start()
-    success, uuid = hook.subscribe_stream_changed(user_id, callback_stream_changed)
-
-
-def get_stream(streamer, quality='best'):
-    url = 'https://www.twitch.tv/' + streamer
-    return streamlink.streams(url)[quality]
-
-
-def download_stream(streamer, path, chunk_size=8192):
-    stream = get_stream(streamer).open()
-    with open(path, 'wb') as f:
-        data = stream.read(chunk_size)
-        while data:
-            try:
-                f.write(data)
-            except OSError as err:
-                print(err)
-                break
-            data = stream.read(chunk_size)
-    stream.close()
-
-
-clientID = ":)"
-clientSecret = ":)"
-users = ['rlly']
-host = ":)"
-port = 42069
-
-download_stream('rlly', '/mnt/media/test.ts')
+    rlly = vod_watcher('rlly', twitch, hook)
