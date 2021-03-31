33e43827283c8f58fd3c8eed42d56b8409d2f8c1
Retrontology <retrontology@hotmail.com>
2021-03-30 20:56:57 -0500

    polish webhook callback

--- vodloader.py
+++ vodloader.py
@@ -4,26 +4,59 @@ from twitchAPI.oauth import UserAuthenticator
 from twitchAPI.types import AuthScope
 import streamlink
 import vodloader_config
+import os
+import _thread
+import time
 
 
 class vod_watcher(object):
 
-    def __init__(self, streamer, twitch, hook, quality='best'):
+    def __init__(self, streamer, twitch, hook, download_dir, quality='best'):
         self.streamer = streamer
         self.quality = quality
+        self.download_dir = download_dir
         self.twitch = twitch
         self.hook = hook
         self.user_id = self.get_user_id()
-        self.webhook_setup()
+        self.check_live()
+        self.webhook_subscribe()
+
+
+    def __del__(self):
+        self.webhook_unsubscribe()
 
 
     def callback_stream_changed(self, uuid, data):
-        print('UUID: ' + uuid)
-        print(data)
+        if data['type'] == 'live':
+            if not self.live:
+                name = data['started_at'] + '.ts'
+                path = os.path.join(self.download_dir, name)
+                _thread.start_new_thread(self.download_stream, (path, ))
+            self.live = True
+        else:
+            self.live = False
 
 
-    def webhook_setup(self):
-        success, uuid = self.hook.subscribe_stream_changed(self.user_id, self.callback_stream_changed)
+    def check_live(self):
+        if self.twitch.get_streams(user_id=self.user_id)['data'][0]['type'] == 'live':
+            self.live = True
+        else:
+            self.live = False
+        return self.live
+
+
+    def webhook_unsubscribe(self):
+        if self.webhook_uuid:
+            self.hook.unsubscribe(self.webhook_uuid)
+
+
+    def webhook_subscribe(self, retry=3):
+        for i in range(retry):
+            success, uuid = self.hook.subscribe_stream_changed(self.user_id, self.callback_stream_changed)
+            if success:
+                self.webhook_uuid = uuid
+                break
+        return success
 
 
     def get_stream(self, quality='best'):
@@ -50,10 +83,34 @@ class vod_watcher(object):
         stream.close()
 
 
-def main():
-    twitch = Twitch(clientID, clientSecret)
+def load_config(filename):
+    config = vodloader_config.vodloader_config(filename)
+    if not config['download']['directory'] or config['download']['directory'] == "":
+        config['download']['directory'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'videos')
+    return config
+
+
+def setup_twitch(client_id, client_secret):
+    twitch = Twitch(client_id, client_secret)
     twitch.authenticate_app([])
-    hook = TwitchWebHook(host, twitch_client_id, port)
+    return twitch
+
+
+def setup_webhook(host, client_id, port, twitch):
+    hook = TwitchWebHook(host, client_id, port)
     hook.authenticate(twitch)
     hook.start()
-    rlly = vod_watcher('rlly', twitch, hook)
+    return hook
+
+
+def main():
+    config = load_config('config.yaml')
+    twitch = setup_twitch(config['twitch']['client_id'], config['twitch']['client_secret'])
+    hook = setup_webhook(config['webhook']['host'], config['twitch']['client_id'], config['webhook']['port'], twitch)
+    vodw = vod_watcher(config['twitch']['streamer'], twitch, hook, config['download']['directory'])
+    while True:
+        time.sleep(600)
+
+
+if __name__ == '__main__':
+    main()
