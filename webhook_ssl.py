a5514479da8a7cece74396828fbe5090a70d4e47
Retrontology <retrontology@hotmail.com>
2021-04-01 20:24:23 -0500

    finish reverse ssl proxy

--- webhook_ssl.py
+++ webhook_ssl.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 import requests
+import json
 
 class proxy_request_handler(BaseHTTPRequestHandler):
 
@@ -8,6 +9,10 @@ class proxy_request_handler(BaseHTTPRequestHandler):
     def __init__(self, target_port, *args, **kwargs):
         self.target_port = target_port
         super(proxy_request_handler, self).__init__(*args, **kwargs)
+
+
+    def do_HEAD(self):
+        self.forward("HEAD")
 
 
     def do_GET(self, body=True):
@@ -19,22 +24,22 @@ class proxy_request_handler(BaseHTTPRequestHandler):
     
 
     def forward(self, req_type):
-        try:
-            url = 'http://{}{}'.format('127.0.0.1:' + str(self.target_port), self.path)
-            req_header = self.headers
-            print(req_header)
-            if req_type == "POST":
-                req_body = self.rfile.read(int(self.headers.get('Content-Length')))
-                print(req_body)
-                resp = requests.post(url, headers=req_header, json=req_body, verify=False)
-            elif req_type == "GET":
-                resp = requests.get(url, headers=req_header, verify=False)
-            self.send_response(resp.status_code)
-            for key in resp.headers.keys():
-                if not key in ['Date', 'Server']:
-                    self.send_header(key, resp.headers[key])
-            self.end_headers()
-            self.wfile.write(resp.content)
-        finally:
-            pass
-            #self.finish()
+        url = 'http://{}{}'.format('127.0.0.1:' + str(self.target_port), self.path)
+        req_header = self.headers
+        print(req_type)
+        print(req_header)
+        if req_type == "POST":
+            req_body = self.rfile.read(int(self.headers.get('Content-Length')))
+            print(req_body)
+            print(req_body.decode())
+            resp = requests.post(url, headers=req_header, json=json.loads(req_body.decode()), verify=False)
+        elif req_type == "GET":
+            resp = requests.get(url, headers=req_header, verify=False)
+        elif req_type == "HEAD":
+            resp = requests.head(url, headers=req_header, verify=False)
+        self.send_response(resp.status_code)
+        for key in resp.headers.keys():
+            if not key in ['Date', 'Server']:
+                self.send_header(key, resp.headers[key])
+        self.end_headers()
+        self.wfile.write(resp.content)
