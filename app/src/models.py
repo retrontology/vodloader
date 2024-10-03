cbb7a81a12e247bafe167c0eccb31e21bb41868e
retrontology <retrontology@hotmail.com>
2024-10-03 01:55:04 -0500

    remove repeated class

--- app/src/models.py
+++ app/src/models.py
@@ -581,63 +581,6 @@ class TwitchAuth(BaseModel):
         else:
             return None
 
-class ClearMsgEvent(BaseModel):
-
-    table_name = 'twitch_clearmsg'
-    table_command = f"""
-        CREATE TABLE IF NOT EXISTS {table_name} (
-            id VARCHAR(36) NOT NULL UNIQUE,
-            channel INT UNSIGNED NOT NULL,
-            user_id INT UNSIGNED NOT NULL,
-            timestamp DATETIME NOT NULL,
-            duration INT UNSIGNED DEFAULT NULL,
-            PRIMARY KEY (id),
-            FOREIGN KEY (channel) REFERENCES {TwitchChannel.table_name}(id)
-        );
-        """
-    
-    id: str
-    channel: int
-    user_id: int
-    timestamp: datetime
-    duration: int
-
-    def __init__(
-            self,
-            id: str,
-            channel: int,
-            user_id: int,
-            timestamp: datetime,
-            duration: int = None,
-        ):
-        self.id = id
-        self.channel = channel
-        self.user_id = user_id
-        self.timestamp = timestamp
-        self.duration = duration
-    
-    @classmethod
-    def from_event(cls, event: Event):
-
-        tags = parse_tags(event)     
-
-        if 'ban-duration' in tags:
-            duration = int(tags['ban-duration'])
-        else:
-            duration = None
-
-        timestamp = float(tags['tmi-sent-ts'])/1000
-        timestamp = datetime.fromtimestamp(timestamp)
-
-        return cls(
-            id = uuid4().__str__(),
-            channel = int(tags['room-id']),
-            user_id = int(tags['target-user-id']),
-            timestamp = timestamp,
-            duration = duration
-        )
-
-
 
 class Message(BaseModel):
 
