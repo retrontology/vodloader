fd5ba3b65166149fddd88fdffade860b776c98d7
retrontology <retrontology@hotmail.com>
2024-10-08 21:26:58 -0500

    extend stream timeouts

--- app/src/video.py
+++ app/src/video.py
@@ -32,7 +32,11 @@ class Video():
         self.stream = self.get_stream()
         
     def get_stream(self, token=None) -> TwitchHLSStream:
-        session = streamlink.Streamlink()
+        session = streamlink.Streamlink(options={
+            'stream-segment-timeout': 30.0,
+            'stream-timeout': 60.0,
+
+        })
         return session.streams(self.url)[self.quality]
 
     async def download_stream(self):
