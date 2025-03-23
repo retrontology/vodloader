b9faf0b98bebcc366e29de052befc43751ddac58
retrontology <retrontology@gmail.com>
2025-03-23 06:23:29 -0500

    remove try catch

--- app/vodloader/api.py
+++ app/vodloader/api.py
@@ -13,65 +13,55 @@ logger = logging.getLogger('vodloader.api')
 @api.route("/channel/<name>", methods=['POST'])
 async def add_channel(name: str):
 
-    try:
+    if 'secret' not in request.headers or request.headers['secret'] != current_app.secret_key:
+        return "Unauthorized", 403
 
-        if 'secret' not in request.headers or request.headers['secret'] != current_app.secret_key:
-            return "Unauthorized", 403
+    name = name.lower()
 
-        name = name.lower()
+    if 'quality' in request.args:
+        quality = request.args['quality']
+    else:
+        quality = 'best'
 
-        if 'quality' in request.args:
-            quality = request.args['quality']
-        else:
-            quality = 'best'
+    channel = await TwitchChannel.get(login=name)
 
-        channel = await TwitchChannel.get(login=name)
+    if channel:
 
-        if channel:
-
-            if not channel.active:
-                await channel.activate()
-                await subscribe(channel)
-
-        else:
-
-            channel = await TwitchChannel.from_name(name, quality)
-
-            if not channel:
-                return "Channel does not exist on Twitch", 400
-
-            await channel.save()
+        if not channel.active:
+            await channel.activate()
             await subscribe(channel)
 
-    except Exception as e:
-        return "Internal error", 500
-    
-    finally:
-        return"success",  200
+    else:
+
+        channel = await TwitchChannel.from_name(name, quality)
+
+        if not channel:
+            return "Channel does not exist on Twitch", 400
+
+        await channel.save()
+        await subscribe(channel)
+
+    return"success",  200
 
 
 @api.route("/channel/<name>", methods=['DELETE'])
 async def delete_channel(name: str):
     
-    try:
-
-        if 'secret' not in request.headers or request.headers['secret'] != current_app.secret_key:
-            return "Unauthorized", 403
-
-        channel = await TwitchChannel.get(login=name)
-
-        if not channel:
-            return "Channel does not exist in database", 403
-        
-        if channel.active:
-            await channel.deactivate()
-            await unsubscribe(channel)
-       
-    except Exception as e:
-        return "Internal error", 500
+    if 'secret' not in request.headers or request.headers['secret'] != current_app.secret_key:
+        return "Unauthorized", 403
     
-    finally:
-        return "success", 200
+    name = name.lower()
+
+    channel = await TwitchChannel.get(login=name)
+
+    if not channel:
+        return "Channel does not exist in database", 403
+    
+    if channel.active:
+        await channel.deactivate()
+        await unsubscribe(channel)
+
+    return "success", 200
 
 
 @api.route("/channels", methods=['GET'])
@@ -79,23 +69,17 @@ async def get_channels():
 
     output = []
     
-    try:
+    if 'secret' not in request.headers or request.headers['secret'] != current_app.secret_key:
+        return "Unauthorized", 403
 
-        if 'secret' not in request.headers or request.headers['secret'] != current_app.secret_key:
-            return "Unauthorized", 403
+    channels = await TwitchChannel.all()
+    output = {
+        'channels': [
+            x for x in channels
+        ]
+    }
 
-        channels = await TwitchChannel.all()
-        output = {
-            'channels': [
-                x for x in channels
-            ]
-        }
-    
-    except Exception as e:
-        return "Internal error", 500
-
-    finally:
-        return output, 200
+    return output, 200
 
 
 def create_api() -> Quart:
