050a64e476bcb367504104a1a16cb74bb85692cc
retrontology <retrontology@gmail.com>
2025-06-06 19:26:57 -0500

    remove self

--- app/vodloader/post.py
+++ app/vodloader/post.py
@@ -13,7 +13,7 @@ import asyncio
 DEFAULT_WIDTH = 320
 
 
-async def remove_original(self, video: VideoFile):
+async def remove_original(video: VideoFile):
     
     if not video.path:
         raise VideoAlreadyRemoved
@@ -24,7 +24,8 @@ async def remove_original(self, video: VideoFile):
     await video.save()
     video.logger.info(f'The original stream file at {path.__str__()} has been deleted')
 
-async def transcode(self, video: VideoFile):
+
+async def transcode(video: VideoFile) -> None:
 
     if not video.ended_at:
         raise VideoFileNotEnded
@@ -32,15 +33,15 @@ async def transcode(self, video: VideoFile):
     if video.transcode_path:
         raise VideoAlreadyTranscoded
     
-    video.logger.info(f'Transcoding {self.path}')
+    video.logger.info(f'Transcoding {video.path}')
     loop = asyncio.get_event_loop()
     video.transcode_path = await loop.run_in_executor(None, _transcode, video)
     await video.save()
-    video.logger.info(f'Finished transcoding {self.path} to {self.transcode_path}')
+    video.logger.info(f'Finished transcoding {video.path} to {video.transcode_path}')
     await remove_original(video)
 
 
-def _transcode(self, video: VideoFile) -> Path:
+def _transcode(video: VideoFile) -> Path:
     transcode_path = video.path.parent.joinpath(f'{video.path.stem}.mp4')
     stream = ffmpeg.input(video.path.__str__())
     stream = ffmpeg.output(stream, transcode_path.__str__(), vcodec='copy')
@@ -48,7 +49,7 @@ def _transcode(self, video: VideoFile) -> Path:
     ffmpeg.run(stream, quiet=True)
     return transcode_path
 
-@staticmethod
+
 def _get_fonts() -> List[FT2Font]:
     systemt_fonts = font_manager.findSystemFonts(fontext='ttf')
     fonts = []
@@ -59,13 +60,15 @@ def _get_fonts() -> List[FT2Font]:
             print(f'Ran into the following exception trying to load the font from {font}: {e}')
     return fonts
 
+
 async def transcode_loop():
     while True:
         video = await VideoFile.get_next_transcode()
         if video:
-            await video.transcode()
+            await transcode(video)
         else:
             await asyncio.sleep(60)
+
 
 class VideoAlreadyTranscoded(Exception): pass
 class VideoAlreadyRemoved(Exception): pass
